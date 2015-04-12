ba6eaee548ba64d4fdf40e325edb606fdfaace37
Richard Olsson <r@richardolsson.se>
2015-04-12 20:30:52 +0200

    Refactor component hierary for better control.
    Add one extra component level to more easily be able to detect click events on
    either the label, the value, or any (the entire node).

--- src/TreeView.jsx
+++ src/TreeView.jsx
@@ -2,31 +2,56 @@ var React = require('react/addons');
 
 var TreeView = React.createClass({
     render: function() {
-        return <TreeNode data={ this.props.data }/>;
+        return (
+            <TreeNodeValue data={ this.props.data }/>
+        );
     }
 });
 
 var TreeNode = React.createClass({
     render: function() {
+        if (this.props.label) {
+            return (
+                <li>
+                    <TreeNodeLabel label={ this.props.label }/>
+                    <TreeNodeValue data={ this.props.value }/>
+                </li>
+            );
+        }
+        else {
+            return <li><TreeNodeValue data={ this.props.value }/></li>;
+        }
+    }
+});
+
+var TreeNodeLabel = React.createClass({
+    render: function() {
+        return (
+            <span className="treeview-label">{ this.props.label }</span>
+        );
+    }
+});
+
+var TreeNodeValue = React.createClass({
+    render: function() {
         if (this.props.data.constructor === Array) {
             return (
-                <ol className="treeview-node-array" start="0">
+                <ol className="treeview-value-array" start="0">
                     {this.props.data.map(function(item, index) {
-                        return <li key={ index }><TreeNode data={ item }/></li>
+                        return (
+                            <TreeNode key={ index } label="" value={ item }/>
+                        );
                     }, this)}
                 </ol>
             )
         }
         else if (this.props.data.constructor === Object) {
             return (
-                <ul className="treeview-node-object">
+                <ul className="treeview-value-object">
                     {Object.keys(this.props.data).map(function(key) {
                         var val = this.props.data[key];
                         return (
-                            <li key={ key }>
-                                <span className="treeview-node-key">{ key }</span>: 
-                                <TreeNode data={ val }/>
-                            </li>
+                            <TreeNode key={ key } label={ key } value={ val }/>
                         );
                     }, this)}
                 </ul>
